876085056ef95c9e2f66b2e415a0dfbbf189ad6a
Luiz Fernando <fernando.spyke@gmail.com>
2025-07-04 22:42:07 -0300

    V 0.0.2

--- Aether-Serpent/Assets/Scripts/Move.cs
+++ Aether-Serpent/Assets/Scripts/Move.cs
@@ -1,10 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Move : MonoBehaviour
 {
     [SerializeField] Vector2 direction;
+    [SerializeField] List<Transform> snakeBodies;
+    [SerializeField] Transform body;
+
+    private void Start()
+    {
+        snakeBodies = new List<Transform>();
+        snakeBodies.Add(transform);
+    }
+
 
     private void Update()
     {
@@ -14,11 +24,15 @@ public class Move : MonoBehaviour
         if (xAxis != 0)
             direction = Vector2.right * xAxis;
         if (yAxis != 0)
-            direction = Vector2.up * yAxis;         
+            direction = Vector2.up * yAxis;
     }
 
     private void FixedUpdate()
     {
+        for (int i = snakeBodies.Count - 1; i > 0; i--)
+        {
+            snakeBodies[i].position = snakeBodies[i - 1].position;
+        }
         moveSnake();
     }
     void moveSnake()
@@ -27,5 +41,20 @@ public class Move : MonoBehaviour
         float roundPosY = Mathf.Round(transform.position.y);
 
         transform.position = new Vector2(roundPosX + direction.x, roundPosY + direction.y);
+    }
+
+    void GrowingSnake()
+    {
+        Transform SpawnBody = Instantiate(body, snakeBodies[snakeBodies.Count - 1].position, Quaternion.identity);
+        snakeBodies.Add(SpawnBody);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        switch (collision.tag)
+        {
+            case "Food":
+                GrowingSnake();
+                break;
+        }
     }
 }
